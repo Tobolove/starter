3b041fb44c3350ccbf0b4f177c1306560fcd258e
Tobias Frei <tobias.frei@vivavis.ch>
2025-05-01 16:19:06 +0200

    handlers

---
+++ app.py
@@ -0,0 +1,76 @@
+import os
+from flask import Flask, jsonify
+from models import setup_db, Person # models.py greift auf DATABASE_URL zu
+from flask_cors import CORS
+from dotenv import load_dotenv # <<<--- 1. Importieren
+
+
+load_dotenv()
+
+def create_app(test_config=None):
+
+    app = Flask(__name__)
+    setup_db(app) # Kann jetzt die geladene DATABASE_URL nutzen
+    CORS(app)
+
+    @app.route('/')
+    def get_greeting():
+        # Greift jetzt auf die Variable aus .env zurück
+        # Besser .get() verwenden, falls die Variable fehlt:
+        excited = os.environ.get('EXCITED', 'false')
+        greeting = "Hello"
+        if excited == 'true':
+            greeting = greeting + "!!!!! You are doing great in this Udacity project."
+        return greeting
+
+    @app.route('/coolkids')
+    def be_cool():
+        return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+    
+
+    @app.route('/get-people')
+    def get_people():
+        try:
+            people_query = Person.query.all()
+            formatted_people = [person.format() for person in people_query]
+
+            return jsonify({"people": formatted_people}), 200
+            
+        except Exception as e:
+            print(f"Error: {e}")
+            return jsonify({"error": "An error occurred while fetching people."}), 500
+
+
+    @app.route('/get-people/<int:person_id>')
+    def get_person(person_id):
+        try:
+            person = Person.query.get(person_id)
+            if person is None:
+                return jsonify({"error": "Person not found."}), 404
+
+            return jsonify({"person": person.format()}), 200
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return jsonify({"error": "An error occurred while fetching hthe person."}), 500
+        
+    @app.route('/get-people/<int:person_id>/catchphrase')
+    def get_person_catchphrase(person_id):
+        try:
+            person = Person.query.get(person_id)
+            if person is None:
+                return jsonify({"error": "Person not found."}), 404
+
+            return jsonify({"catchphrase": person.catchphrase}), 200
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return jsonify({"error": "An error occurred while fetching the person's catchphrase."}), 500
+    return app   
+    
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
